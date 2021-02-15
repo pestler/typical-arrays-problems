dee440ab180475211972076528af6cace81f9b7a
olezhenka <oleh@tut.by>
2021-02-15 23:03:27 +0300

    feat : modifu file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arr) {
+    if (arr === undefined || arr.length === 0) return 0
+    else return Math.min.apply(null, arr)
 }
-
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr) {
+    if (arr === undefined || arr.length === 0) return 0
+    else return Math.max.apply(null, arr)
 }
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arr) {
+    if (arr === undefined || arr.length === 0) return 0
+    else let result = arr.reduce(function (sum, current) {
+        return sum + current;
+    }, 0)
+    return arr = result / arr.length
 }
