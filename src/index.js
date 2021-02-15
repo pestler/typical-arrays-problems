1c728737dcbe57971d56c9d9cae59585683a89f4
olezhenka <oleh@tut.by>
2021-02-15 23:27:45 +0300

    feat: fix errors return

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 exports.min = function min(array) {
     if (array === undefined || array.length === 0) return 0
-    else return Math.min.apply(null, array)
+    return Math.min.apply(null, array)
 };
 exports.max = function max(array) {
     if (array === undefined || array.length === 0) return 0
-    else return Math.max.apply(null, array)
+    return Math.max.apply(null, array)
 };
 exports.avg = function avg(array) {
     if (array === undefined || array.length === 0) return 0
-    else let result = array.reduce(function (sum, current) {
+    let result = array.reduce(function (sum, current) {
         return sum + current;
     }, 0)
     return array = result / array.length
